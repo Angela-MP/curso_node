460cefe2e6ea3a5f0a138f292757a84ae4158902
Angela Montes Prieto <anngylitha_27@hotmail.es>
2021-11-27 20:08:00 -0600

    Evidencia hasta el video Curso Nodejs+Express S3-P4

---
+++ routes/usuarios.js
@@ -0,0 +1,18 @@
+const {Router} = require("express");
+const router = Router();
+
+//localhost:3001/usuarios
+router.get("/", function(req, res){
+    res.json({msg: "Hola a todos desde GET" });
+});
+router.post("/", function(req, res){
+    res.status(201).json({msg: "Hola a todos desde POST" });
+});
+router.put("/", function(req, res){
+    res.status(400).json({msg: "Hola a todos desde PUT" });
+});
+router.delete("/", function(req, res){
+    res.status(500).json({msg: "Hola a todos desde DELETE" });
+});
+
+module.exports = router;
